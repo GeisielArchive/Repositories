6509c316921b233ee8c0b18473c7ecfa30137903
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-05-25 16:38:13 -0300

    repository controller with functions create and destroy

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import HelloController from "./controllers/HelloController";
 import UsersController from "./controllers/UsersController";
+import RepositoryController from "./controllers/RepositoryController";
 
 const routes = new Router();
 
@@ -12,5 +13,10 @@ routes.get("/users/:id", UsersController.show);
 routes.post("/users", UsersController.create);
 routes.put("/users/:id", UsersController.update);
 routes.delete("/users/:id", UsersController.destroy);
+
+// Repository
+routes.get("/users/:user_id/repository", RepositoryController.index);
+routes.post("/users/:user_id/repository", RepositoryController.create);
+routes.delete("/users/:user_id/repository", RepositoryController.destroy);
 
 export default routes;
